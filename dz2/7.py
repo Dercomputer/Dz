cb8c8b1c2859bdb22b967b8fdacebe53608653da
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-09-30 13:22:47 +0300

    Update 7.py

--- dz2/7.py
+++ dz2/7.py
@@ -2,7 +2,7 @@ from random import randint
 
 x = randint(0, 100)
 y = int(input("Введите число от 1 до 100: "))
-while y !=x:
+while y != x:
     if x > y:
         print("Загаданное число больше!")
         y = int(input("Введите число от 1 до 100: "))
