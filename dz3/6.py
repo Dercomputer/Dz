106067dd6943cdd537669da777cba67d0bd8e49b
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-13 18:54:56 +0300

    Update 6.py

--- dz3/6.py
+++ dz3/6.py
@@ -1,12 +1,14 @@
-n = int(input("Число: "))
+def prost(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
 
 
-def prost(n):
-    if (n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n % 7 == 0 or n % 11 == 0
-        or n % 13 == 0) and n not in [2, 3, 5, 7, 11, 13] or n == 1:
-        print(False)
-    else:
-        print(True)
-
-
-prost(n)
+number = int(input())
+if prost(number):
+    print("Простое число")
+else:
+    print("Не простое число")
