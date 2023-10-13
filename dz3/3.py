01b3b365a8f327a275dc705b3afea12a63a347a6
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-13 18:54:20 +0300

    Update 3.py

--- dz3/3.py
+++ dz3/3.py
@@ -1,11 +1,13 @@
 def fib(n):
-   a = 1
-   b = 1
-   for i in range(1, n+1):
-    if i == 1 or i == 2:
-          print(a)
-    elif i > 2:
-        a, b = b, a+b
-        print(b)
+    a = 1
+    b = 1
+    for i in range(1, n+1):
+        if i in [1, 2]:
+            print(a, end=" ")
+        elif i > 2:
+            a, b = b, a+b
+            print(b, end=" ")
+
+
 n = int(input("Число: "))
 fib(n)
