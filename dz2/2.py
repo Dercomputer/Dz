0ee9a6b8e7b910b9994184d6d733877c1a420669
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-13 22:21:01 +0300

    Update 2.py

--- dz2/2.py
+++ dz2/2.py
@@ -1,5 +1,6 @@
 def maxnumber(a):
-    a.sort(key=lambda x: x * 3, reverse=True)
+    m = len(max(a, key=len))
+    a.sort(key=lambda x: x * m, reverse=True)
     result = ''.join(a)
     result = int(result)
     return result
