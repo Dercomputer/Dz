300e87bf953b35957556cb185a9bb72dfb990a24
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-14 21:23:33 +0300

    Update 7.py

--- dz3/7.py
+++ dz3/7.py
@@ -2,10 +2,10 @@ from collections import Counter
 
 
 def pabtext(text):
-    P = max(text, key=len)
+    p = max(text, key=len)
     o = Counter(text)  # create dict, elements - key and count - value
     c = o.most_common(1)[0][0]
-    return P, c
+    return p, c
 
 text = input().split()
 
