b9de8ed542955d26d4ad0af1d7f3348dc2b4fcf8
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-14 13:43:43 +0300

    Update 2.py

--- dz3/2.py
+++ dz3/2.py
@@ -1,15 +1,14 @@
-n = int(input("Месяц: "))
-
-
 def Season(n):
     if n in [1, 2, 12]:
-        print("Зима")
-    elif n in [3, 4, 5]:
-        print("Весна")
-    elif n in [6, 7, 8]:
-        print("Лето")
-    elif n in [9, 10, 11]:
-        print("Осень")
+        return "Зима"
+    if n in [3, 4, 5]:
+        return "Весна"
+    if n in [6, 7, 8]:
+        return "Лето"
+    if n in [9, 10, 11]:
+        return "Осень"
+    return "Ошибка"
 
-
-Season(n)
+     
+n = int(input("Месяц: "))
+print(Season(n))
