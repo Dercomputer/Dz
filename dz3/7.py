d8a87b699fc0f1ee9adbc365c5806258d44e089f
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-10-14 13:45:53 +0300

    Update 7.py

--- dz3/7.py
+++ dz3/7.py
@@ -1,11 +1,12 @@
 from collections import Counter
 
-text = input().split()
+
 def pabtext(text):
     P = max(text, key=len)
-    c = max(Counter(text))  # create dict, elements - key and count - value
+    o = (Counter(text))  # create dict, elements - key and count - value
+    c = max(o, key=lambda x: o[x])
+    print(Counter(text))
     print("Самое длинное: " + P + ", самое частое: " + c)
-
+text = input().split()
 
 pabtext(text)
-
