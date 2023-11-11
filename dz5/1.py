008b5360f9027067418ec5b134653e0fe5a3ceb2
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-11-11 18:11:27 +0300

    Create 1.py

---
+++ dz5/1.py
@@ -0,0 +1,20 @@
+from typing import Sequence, Any
+
+
+def bin_search(pos: Sequence, el: Any) -> int | None:
+    min_index, max_index = 0, len(pos) - 1
+    result = None
+    while min_index <= max_index:
+        index = (min_index + max_index) // 2
+        if pos[index] == el:
+            result = index
+            max_index = index - 1
+        elif pos[index] > el:
+            max_index = index - 1
+        else:
+            min_index = index + 1
+    return result
+
+
+if __name__ == '__main__':
+    print(bin_search([1, 2, 3, 4, 5], 2))
