620e4738c2fec075d3ed3d73565a02cdc4cbd516
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2023-12-09 13:48:47 +0300

    Create bfs.py

---
+++ dz7/bfs.py
@@ -0,0 +1,25 @@
+from collections import deque
+from typing import Dict, List
+
+
+def check(node, target):
+    return node == target
+
+def bfs(graph: Dict[int, List[int]], start: int, target: int, function) -> int | None:
+    visited = set()
+    queue = deque([(start, 0)])
+
+    while queue:
+        node, distance = queue.popleft()
+        if function(node, target):
+            return distance
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append((neighbor, distance + 1))
+
+    return None
+
+
+if __name__ == '__main__':
+    print(bfs({0: [1, 4], 1: [77, 55], 4: [99, 666]}, 0, 666, check))
