9914dde2cfa9a1aedff88dded0f4195c005f9128
Dercomputer <143877358+Dercomputer@users.noreply.github.com>
2024-03-02 14:40:39 +0300

    Update Matrixxx.py

--- OOP/Matrix/Matrixxx.py
+++ OOP/Matrix/Matrixxx.py
@@ -45,52 +45,30 @@ class Matrix:
 
 class Matrix3x3(Matrix):
 
-    def __init__(self, elements=[[0, 0, 0], [0, 0, 0], [0, 0, 0]]):
+    def __init__(self, elements: list = None):
+        if elements is None:
+            elements = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         super().__init__(3, 3, elements)
 
     def input_matrix(self):
         super()._matrix_elements()
 
     def determinant(self):
-        return self.elements[0][0] * self.elements[1][1] * self.elements[2][2] + self.elements[0][1] * \
-            self.elements[1][2] * self.elements[2][0] + self.elements[0][2] * self.elements[1][0] * \
-            self.elements[2][1] - self.elements[0][2] * self.elements[1][1] * self.elements[2][0] - \
-            self.elements[0][0] * self.elements[1][2] * self.elements[2][1] - self.elements[1][0] * \
-            self.elements[0][1] * self.elements[2][2]
+        return self.elements[0][0] * self.elements[1][1] * self.elements[2][2] + self.elements[0][1] \
+            * self.elements[1][2] * self.elements[2][0] + self.elements[0][2] * self.elements[1][0]  \
+            * self.elements[2][1] - self.elements[0][2] * self.elements[1][1] * self.elements[2][0]  \
+            - self.elements[0][0] * self.elements[1][2] * self.elements[2][1] - self.elements[1][0]  \
+            * self.elements[0][1] * self.elements[2][2]
 
 
-# if __name__ == "__main__":
-#     #n1 = Matrix(2, 2, [[1, 1], [1, 1]])
-#     #n2 = Matrix(2, 2, [[1, 1], [1, 1]])
-#     # n1.input_matrix()
-#     # n2.input_matrix()
-#     # print(n1.determinant())
-#     # print(n1.elements,n2.elements)
-#     # m = Matrix(2, 2, [[1, 2], [5, 6]])
-#     # print(m.elements)
-#     #print(n1 + n2)
-#     p = Matrix3x3()
-#     p.input_matrix()
-#     print(p)
+if __name__ == "__main__":
+    #     #n1 = Matrix(2, 2, [[1, 1], [1, 1]])
+    #     #n2 = Matrix(2, 2, [[1, 1], [1, 1]])
+    #     #print(n1 + n2)
+    p = Matrix3x3()
+    p.input_matrix()
+    print(p.determinant())
 
-
-class PMatrix:
-
-    def __init__(self, lines: int = 0, pillars: int = 0, elements: list = None):
-        if elements is None:
-            elements = []
-        self.lines = lines
-        self.pillars = pillars
-        self.elements = elements
-
-    def __str__(self):
-        output = ''
-        for j in range(len(self.elements)):
-            output += ' '.join(str(self.elements[j]))
-            if len(output) % self.pillars == 0:
-                output += '\n'
-                print(output)
-        return output
 
 
 # m = PMatrix(2, 2, [1, 1, 1, 1])
